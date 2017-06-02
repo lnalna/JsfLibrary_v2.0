a972f0158604a4b1aeafca84c71c24ae0ecba155
Nikolay <lnalna@yandex.ru>
2017-06-02 11:39:51 +0300

    Создан класс ru.javabegin.training.web.db.Database  для соединения с базой данных Library;

---
+++ src/java/ru/javabegin/training/web/db/Database.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.javabegin.training.web.db;
+
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+
+/**
+ *
+ * @author nik
+ */
+public class Database {
+    
+    private static Connection conn;
+    private static InitialContext ic;
+    private static DataSource ds;
+ 
+    public static Connection getConnection() {
+        
+        try {
+             
+            ic = new InitialContext();
+            ds = (DataSource) ic.lookup("java:comp/env/jdbc/Library");
+            conn = ds.getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NamingException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return conn;
+    }
+}
